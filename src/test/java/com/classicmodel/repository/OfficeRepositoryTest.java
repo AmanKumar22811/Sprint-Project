1f71ca877d1a8e07a6960eedf86a28876cb9f6ca
HimanshuSingh-1 <hsinghsoftwares@gmail.com>
2026-04-29 21:43:25 +0530

    More Test Cases Added in OfficeRepositoryTest

--- src/test/java/com/classicmodel/repository/OfficeRepositoryTest.java
+++ src/test/java/com/classicmodel/repository/OfficeRepositoryTest.java
@@ -63,4 +63,39 @@ class OfficeRepositoryTest {
         assertTrue(count > 0);
     }
 
+    @Test
+    void testAddAndDeleteOffice_succeeds() {
+        Office o = new Office();
+        o.setOfficeCode("99");
+        o.setCity("TestCity");
+        o.setPhone("+1 650 000 0000");
+        o.setAddressLine1("123 Test Ave");
+        o.setCountry("USA");
+        o.setPostalCode("00000");
+        o.setTerritory("NA");
+
+        officeRepo.save(o);
+        assertTrue(officeRepo.findById("99").isPresent());
+        officeRepo.deleteById("99");
+        assertFalse(officeRepo.findById("99").isPresent());
+    }
+
+    @Test
+    void testUpdateOfficeCity_succeeds() {
+        Optional<Office> opt = officeRepo.findById("1");
+        assertTrue(opt.isPresent());
+        Office o = opt.get();
+        String original = o.getCity();
+        o.setCity("Updated City");
+        officeRepo.save(o);
+        assertEquals("Updated City", officeRepo.findById("1").get().getCity());
+        o.setCity(original);
+        officeRepo.save(o);
+    }
+
+    @Test
+    void testGetOfficesByCityIn_returnsMultiple() {
+        List<Office> result = officeRepo.findByCityIn(List.of("Tokyo", "London"));
+        assertNotNull(result);
+    }
 }
