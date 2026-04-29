ef4d0f6f143f16d0683238e3e72a3a2be84b7d28
AmanKumar <aman228115918@gmail.com>
2026-04-29 23:36:19 +0530

    resolve some issue in employee repo

--- src/main/java/com/classicmodel/repository/EmployeeRepository.java
+++ src/main/java/com/classicmodel/repository/EmployeeRepository.java
@@ -2,8 +2,8 @@ package com.classicmodel.repository;
 
 import com.classicmodel.entity.Employee;
 import com.classicmodel.projection.EmployeeProjection;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
@@ -12,15 +12,15 @@ import java.util.Optional;
 @RepositoryRestResource(path = "employees", excerptProjection = EmployeeProjection.class)
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    List<Employee> findByLastName(String lastName);
+	List<Employee> findByLastName(String lastName);
 
-    List<Employee> findByJobTitle(String jobTitle);
+	List<Employee> findByJobTitle(String jobTitle);
 
-    List<Employee> findByOffice_OfficeCode(String officeCode);
+	List<Employee> findByOffice_OfficeCode(String officeCode);
 
-    List<Employee> findByOffice_City(String city);
+	List<Employee> findByOffice_City(String city);
 
-    Optional<Employee> findByEmail(String email);
+	Optional<Employee> findByEmail(String email);
 
-    List<Employee> findByManager_EmployeeNumber(Integer managerNumber);
+	List<Employee> findByManager_EmployeeNumber(Integer managerNumber);
 }
