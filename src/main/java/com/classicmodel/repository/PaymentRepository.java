78557f5cfe5aa6ae305b576165b72f42110e6219
aayushi1139 <lalitahaj@gmail.com>
2026-04-29 21:16:58 +0530

    Payment projection and Repository (#11)
    * Added Payment Entity
    
    * PaymentId embeddable class for composite primary key
    
    * Added Payment Projection
    
    * Added Payment Repository

---
+++ src/main/java/com/classicmodel/repository/PaymentRepository.java
@@ -0,0 +1,21 @@
+package com.classicmodel.repository;
+
+import com.classicmodel.entity.Payment;
+import com.classicmodel.entity.PaymentId;
+import com.classicmodel.projection.PaymentProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RepositoryRestResource(path = "payments", excerptProjection = PaymentProjection.class)
+public interface PaymentRepository extends JpaRepository<Payment, PaymentId> {
+
+    List<Payment> findByCustomer_CustomerNumber(Integer customerNumber);
+
+    List<Payment> findByPaymentDate(LocalDate paymentDate);
+
+    List<Payment> findById_CheckNumber(String checkNumber);
+
+}
