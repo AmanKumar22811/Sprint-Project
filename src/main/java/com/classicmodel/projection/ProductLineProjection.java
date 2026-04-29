a35630afada5bd27cba3d92a1133edbce603a482
dishashukla24 <dishashukla0110@gmail.com>
2026-04-29 21:32:19 +0530

    added productline projection

---
+++ src/main/java/com/classicmodel/projection/ProductLineProjection.java
@@ -0,0 +1,12 @@
+package com.classicmodel.projection;
+
+import com.classicmodel.entity.ProductLine;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "productLineExcerpt", types = {ProductLine.class})
+public interface ProductLineProjection {
+    String getProductLine();
+    String getTextDescription();
+    String getHtmlDescription();
+    String getImageUrl();
+}
