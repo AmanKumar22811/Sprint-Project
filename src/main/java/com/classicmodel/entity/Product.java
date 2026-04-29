c19ed7231c7ae9ccf6f38c5d258c7c36ea1eda4d
dishashukla24 <dishashukla0110@gmail.com>
2026-04-29 17:58:38 +0530

    added product entity

---
+++ src/main/java/com/classicmodel/entity/Product.java
@@ -0,0 +1,80 @@
+package com.classicmodel.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @Column(name = "productCode")
+    @NotBlank
+    private String productCode;
+
+    @Column(name = "productName")
+    @NotBlank
+    private String productName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "productLine")
+    private ProductLine productLineEntity;
+
+    @Column(name = "productScale")
+    @NotBlank
+    private String productScale;
+
+    @Column(name = "productVendor")
+    @NotBlank
+    private String productVendor;
+
+    @Column(name = "productDescription", columnDefinition = "TEXT")
+    @NotBlank
+    private String productDescription;
+
+    @Column(name = "quantityInStock")
+    @NotNull
+    private Short quantityInStock;
+
+    @Column(name = "buyPrice")
+    @NotNull
+    private BigDecimal buyPrice;
+
+    @Column(name = "MSRP")
+    @NotNull
+    private BigDecimal MSRP;
+
+   
+    public Product() {}
+
+    public String getProductCode() { return productCode; }
+    public void setProductCode(String productCode) { this.productCode = productCode; }
+
+    public String getProductName() { return productName; }
+    public void setProductName(String productName) { this.productName = productName; }
+
+    public ProductLine getProductLineEntity() { return productLineEntity; }
+    public void setProductLineEntity(ProductLine productLineEntity) { this.productLineEntity = productLineEntity; }
+
+    public String getProductScale() { return productScale; }
+    public void setProductScale(String productScale) { this.productScale = productScale; }
+
+    public String getProductVendor() { return productVendor; }
+    public void setProductVendor(String productVendor) { this.productVendor = productVendor; }
+
+    public String getProductDescription() { return productDescription; }
+    public void setProductDescription(String productDescription) { this.productDescription = productDescription; }
+
+    public Short getQuantityInStock() { return quantityInStock; }
+    public void setQuantityInStock(Short quantityInStock) { this.quantityInStock = quantityInStock; }
+
+    public BigDecimal getBuyPrice() { return buyPrice; }
+    public void setBuyPrice(BigDecimal buyPrice) { this.buyPrice = buyPrice; }
+
+    public BigDecimal getMSRP() { return MSRP; }
+    public void setMSRP(BigDecimal MSRP) { this.MSRP = MSRP; }
+
+}
