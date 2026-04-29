b4a5f305cf7397ba09536149bffc049036f038e1
aayushi1139 <lalitahaj@gmail.com>
2026-04-29 20:05:16 +0530

    Added Payment Entity (#6)
    * Added Payment Entity
    
    * PaymentId embeddable class for composite primary key

---
+++ src/main/java/com/classicmodel/entity/PaymentId.java
@@ -0,0 +1,60 @@
+package com.classicmodel.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class PaymentId implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Column(name = "customerNumber")
+    private Integer customerNumber;
+
+    @Column(name = "checkNumber")
+    private String checkNumber;
+
+    public PaymentId() {}
+
+    public PaymentId(Integer customerNumber, String checkNumber) {
+        this.customerNumber = customerNumber;
+        this.checkNumber = checkNumber;
+    }
+
+    public Integer getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(Integer customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCheckNumber() {
+        return checkNumber;
+    }
+
+    public void setCheckNumber(String checkNumber) {
+        this.checkNumber = checkNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PaymentId)) return false;
+        PaymentId that = (PaymentId) o;
+        return Objects.equals(customerNumber, that.customerNumber) &&
+               Objects.equals(checkNumber, that.checkNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(customerNumber, checkNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "PaymentId{customerNumber=" + customerNumber + ", checkNumber='" + checkNumber + "'}";
+    }
+}
