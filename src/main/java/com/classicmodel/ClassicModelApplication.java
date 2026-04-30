8ca49e7f34ae5f010c28c6a4e9f6d990b9934ce9
Akshaygupta7890 <akshaygupta8086@gmail.com>
2026-04-30 09:47:10 +0530

    commit

---
+++ src/main/java/com/classicmodel/ClassicModelApplication.java
@@ -0,0 +1,12 @@
+package com.classicmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassicModelApplication.class, args);
+    }
+}
